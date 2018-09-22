2e3faedaa63e16aa1a54595ce228e66ad1a4e996
Elena <marzi@student.chalmers.se>
2018-09-22 20:40:41 +0200

    more comments

--- parse.h
+++ parse.h
@@ -2,12 +2,14 @@
 /*
  * info om varje program. Det finns en variabel pgmlist som är array of strängar. Där den försata elementet är
  * namnet på programmet och den andra är argumentet till praogrammet. Testat detta igenom att skriva t.ex ls på shellen
- * Det visas stdin: <none>
- *           stdout:<none>
- *           bg: no
+ * Det visas stdin: <none>   --> jag redirectar inte stdin  
+ *           stdout:<none>   --> jag redirectar inte stdout
+ *           bg: no          --> jag vill inte starta processen i bakgrunden
  *           [ls]
  * testa med ls < a visar stdin:a vilket betyder att parse förstår att input ska komma från filen a . Titta i printCommand!
- * 
+ * testa med cat < abdc > def & -> vi starta programmet cat som läser från en fil  abdc , allt utskrift kommer till en annan 
+ * fil som heter def och programmet startar i bakgrunden. VI vet att det funkar eftersom printCommand skriver ut rätt sak på rät plats. 
+ * Vi behöver inte skriva kod för att parsa!!! (good news!)
  */
 typedef struct c {
   char **pgmlist;
